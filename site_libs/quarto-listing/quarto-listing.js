eb80eeef4072dbdebf1e1cfcc72b81265d509c63
statasaurus <55274484+statasaurus@users.noreply.github.com>
2025-03-19 09:33:43 +0000

    Deploying to gh-pages from @ PSIAIMS/CAMIS@2f71460ce4a46f84dddd037deab18d6e69db0818 🚀

--- site_libs/quarto-listing/quarto-listing.js
+++ site_libs/quarto-listing/quarto-listing.js
@@ -2,8 +2,6 @@ const kProgressiveAttr = "data-src";
 let categoriesLoaded = false;
 
 window.quartoListingCategory = (category) => {
-  // category is URI encoded in EJS template for UTF-8 support
-  category = decodeURIComponent(atob(category));
   if (categoriesLoaded) {
     activateCategory(category);
     setCategoryHash(category);
@@ -17,9 +15,7 @@ window["quarto-listing-loaded"] = () => {
   if (hash) {
     // If there is a category, switch to that
     if (hash.category) {
-      // category hash are URI encoded so we need to decode it before processing
-      // so that we can match it with the category element processed in JS
-      activateCategory(decodeURIComponent(hash.category));
+      activateCategory(hash.category);
     }
     // Paginate a specific listing
     const listingIds = Object.keys(window["quarto-listings"]);
@@ -62,10 +58,7 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
   );
 
   for (const categoryEl of categoryEls) {
-    // category needs to support non ASCII characters
-    const category = decodeURIComponent(
-      atob(categoryEl.getAttribute("data-category"))
-    );
+    const category = categoryEl.getAttribute("data-category");
     categoryEl.onclick = () => {
       activateCategory(category);
       setCategoryHash(category);
@@ -215,9 +208,7 @@ function activateCategory(category) {
 
   // Activate this category
   const categoryEl = window.document.querySelector(
-    `.quarto-listing-category .category[data-category='${btoa(
-      encodeURIComponent(category)
-    )}']`
+    `.quarto-listing-category .category[data-category='${category}'`
   );
   if (categoryEl) {
     categoryEl.classList.add("active");
@@ -240,9 +231,7 @@ function filterListingCategory(category) {
         list.filter(function (item) {
           const itemValues = item.values();
           if (itemValues.categories !== null) {
-            const categories = decodeURIComponent(
-              atob(itemValues.categories)
-            ).split(",");
+            const categories = itemValues.categories.split(",");
             return categories.includes(category);
           } else {
             return false;
